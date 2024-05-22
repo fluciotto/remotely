0234eed2a0b2c7e6bccf9e20bc29c8266a3bb476
fluciotto <iivoxe@gmail.com>
2024-05-22 18:37:48 -0300

    TLDR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
-import { Box, TextField } from '@mui/material';
-import Image from 'next/image';
+"use client";
+
+import { Button, Stack, TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function () {
+  const [host, setHost] = useState("192.168.1.2");
+  const [port, setPort] = useState(3389);
+  const [username, setUsername] = useState("usename");
+  const [password, setPassword] = useState("password");
   return (
-    <Box>
-      <TextField value={'lol'} />
-    </Box>
+    <Stack gap={2}>
+      <Stack direction="row" gap={2}>
+        <TextField
+          label="host"
+          value={host}
+          onChange={(event) => setHost(event.target.value)}
+        />
+        <TextField
+          type="number"
+          label="port"
+          value={port}
+          onChange={(event) => setPort(parseInt(event.target.value))}
+        />
+      </Stack>
+      <TextField
+        label="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <TextField
+        type="password"
+        label="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <Button>Connect</Button>
+    </Stack>
   );
 }

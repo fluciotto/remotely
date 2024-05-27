da0b467a745532dc328b516cfc8f738a1ef7a132
francois <francois@asi-dom.fr>
2024-05-27 14:25:27 -0300

    TLDR

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["freerdp3"],
+  },
   webpack: (config, context) => {
     // console.log(config)
+    // config.externals = [...config.externals, 'freerdp3'];
     return config;
   },
 };

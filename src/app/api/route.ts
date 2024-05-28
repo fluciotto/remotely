be3f7f7bccb24d939eb97479b8accc3ee7f27835
exovii <iivoxe@gmail.com>
2024-05-27 22:36:17 -0300

    TLDR

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,53 @@
 import { NextRequest } from "next/server";
 import { Session } from "freerdp3";
+import { PassThrough } from "stream";
+
+const makeStream = <T extends Record<string, unknown>>(
+  generator: AsyncGenerator<T, void, unknown>
+) => {
+  const encoder = new TextEncoder();
+  return new ReadableStream<any>({
+    async start(controller) {
+      for await (let chunk of generator) {
+        const chunkData = encoder.encode(JSON.stringify(chunk));
+        controller.enqueue(chunkData);
+      }
+      controller.close();
+    },
+  });
+};
+
+class StreamingResponse extends Response {
+  constructor(res: PassThrough, init?: ResponseInit) {
+    // constructor(res: ReadableStream<any>, init?: ResponseInit) {
+    // constructor(res: WritableStream<any>, init?: ResponseInit) {
+    super(res as any, {
+      ...init,
+      status: 200,
+      headers: {
+        ...init?.headers,
+      },
+    });
+  }
+}
+
+type Item = {
+  key: string;
+  value: string;
+};
+
+async function* fetchItems(): AsyncGenerator<Item, void, unknown> {
+  const sleep = async (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  for (let i = 0; i < 10; ++i) {
+    await sleep(1000);
+    yield {
+      key: `key${i}`,
+      value: `value${i}`,
+    };
+  }
+}
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
@@ -38,6 +86,8 @@ export async function GET(req: NextRequest) {
     return new Response(undefined, { status: 400 });
   }
 
+  const stream = new PassThrough();
+
   const client = new Session({
     host,
     port,
@@ -45,6 +95,9 @@ export async function GET(req: NextRequest) {
     password,
     domain: domain || undefined,
     // certIgnore: true,
+    width: 512,
+    height: 512,
+    // bitsPerPixel: 8,
 
     // enablePerf: true,
     // autoLogin: true,
@@ -55,13 +108,19 @@ export async function GET(req: NextRequest) {
   })
     .on("connect", function () {
       console.log("CONNECTED");
-      // client.
+      // stream = new WritableStream();
     })
     .on("close", function () {
       console.log("CLOSED");
+      // reader.close();
+      // writer.close();
+      stream.end();
     })
     .on("bitmap", function (bitmap: any) {
       console.log("BITMAP", bitmap);
+      // writer.getWriter().write(bitmap);
+      stream.write(Buffer.from(JSON.stringify(bitmap)));
+      // stream.write(Buffer.from(JSON.stringify({ lol: "ilol" })));
     })
     .on("error", function (err: any) {
       console.log("error", err);
@@ -69,7 +128,11 @@ export async function GET(req: NextRequest) {
     .on("ERROR", function (err: any) {
       console.log("ERRRROOOOOOOR");
     });
-  // console.log(client);
   client.connect();
-  return new Response(undefined, { status: 200 });
+
+  // return new Response(undefined, { status: 200 });
+
+  // const stream = makeStream(fetchItems());
+  const response = new StreamingResponse(stream);
+  return response;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
 import { Button, Stack, TextField } from "@mui/material";
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+async function* streamingFetch(input: RequestInfo | URL, init?: RequestInit) {
+  const response = await fetch(input, init);
+  const reader = response.body?.getReader();
+  if (!reader) {
+    return;
+  }
+  const decoder = new TextDecoder("utf-8");
+
+  for (;;) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    try {
+      const v = decoder.decode(value);
+      // console.log("value", v);
+      yield decoder.decode(value);
+      // console.log("value", value);
+      // yield value;
+    } catch (e: any) {
+      console.warn(e.message);
+    }
+  }
+}
 
 export default function () {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   // const [host, setHost] = useState("192.168.122.227");
   const [host, setHost] = useState("192.168.122.120");
   const [port, setPort] = useState(3389);
@@ -12,13 +36,40 @@ export default function () {
   const [domain, setDomain] = useState("");
 
   const connect = async () => {
-    const url = new URL("/api/", document.location.origin)
-    url.searchParams.append("host", host)
-    url.searchParams.append("port", port.toString())
-    url.searchParams.append("username", username)
-    url.searchParams.append("password", password)
-    url.searchParams.append("domain", domain)
-    await fetch(url);
+    const url = new URL("/api/", document.location.origin);
+    url.searchParams.append("host", host);
+    url.searchParams.append("port", port.toString());
+    url.searchParams.append("username", username);
+    url.searchParams.append("password", password);
+    url.searchParams.append("domain", domain);
+    // await fetch(url);
+    while (true) {
+      const it = streamingFetch(url);
+      for await (let value of it) {
+        try {
+          console.log("v", value);
+          const chunk = JSON.parse(value);
+          console.log("chunk", chunk.buffer.data.length);
+          const lol = new Uint8ClampedArray(4 * chunk.w * chunk.h);
+          for (let i = 0; i < chunk.buffer.data.length; i += 4) {
+            lol[i + 0] = chunk.buffer.data[i];
+            lol[i + 1] = chunk.buffer.data[i + 1];
+            lol[i + 2] = chunk.buffer.data[i + 2];
+            lol[i + 3] = chunk.buffer.data[i + 3];
+          }
+          console.log("lol", lol);
+          // var buffer32 = new Uint32Array(chunk.buffer),
+          //   len = buffer32.length;
+          // while (len--) buffer32[len] = chunk.buffer[len];
+          canvasRef.current
+            ?.getContext("2d")
+            ?.putImageData(new ImageData(lol, chunk.w, chunk.h), chunk.x, chunk.y);
+          // setData((prev) => [...prev, chunk]);
+        } catch (e: any) {
+          console.warn(e.message);
+        }
+      }
+    }
   };
 
   return (
@@ -55,6 +106,8 @@ export default function () {
         onChange={(event) => setDomain(event.target.value)}
       />
       <Button onClick={() => connect()}>Connect</Button>
+
+      <canvas ref={canvasRef} width={1024} height={768} />
     </Stack>
   );
 }

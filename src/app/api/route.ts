51f7c8dcf633e082a68b913bd116de821dcb2e93
francois <francois@asi-dom.fr>
2024-05-28 14:45:16 -0300

    TLDR

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,25 +2,28 @@ import { NextRequest } from "next/server";
 import { Session } from "freerdp3";
 import { PassThrough } from "stream";
 
-const makeStream = <T extends Record<string, unknown>>(
-  generator: AsyncGenerator<T, void, unknown>
-) => {
-  const encoder = new TextEncoder();
-  return new ReadableStream<any>({
-    async start(controller) {
-      for await (let chunk of generator) {
-        const chunkData = encoder.encode(JSON.stringify(chunk));
-        controller.enqueue(chunkData);
-      }
-      controller.close();
-    },
-  });
-};
+// const makeStream = <T extends Record<string, unknown>>(
+//   generator: AsyncGenerator<T, void, unknown>
+// ) => {
+//   const encoder = new TextEncoder();
+//   return new ReadableStream<any>({
+//     async start(controller) {
+//       for await (let chunk of generator) {
+//         const chunkData = encoder.encode(JSON.stringify(chunk));
+//         controller.enqueue(chunkData);
+//       }
+//       controller.close();
+//     },
+//   });
+// };
 
 class StreamingResponse extends Response {
-  constructor(res: PassThrough, init?: ResponseInit) {
-    // constructor(res: ReadableStream<any>, init?: ResponseInit) {
-    // constructor(res: WritableStream<any>, init?: ResponseInit) {
+  constructor(
+    // res: ReadableStream<any>,
+    // res: WritableStream<any>,
+    res: PassThrough,
+    init?: ResponseInit
+  ) {
     super(res as any, {
       ...init,
       status: 200,
@@ -31,49 +34,67 @@ class StreamingResponse extends Response {
   }
 }
 
-type Item = {
-  key: string;
-  value: string;
-};
+// type Item = {
+//   key: string;
+//   value: string;
+// };
 
-async function* fetchItems(): AsyncGenerator<Item, void, unknown> {
-  const sleep = async (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
+// async function* fetchItems(): AsyncGenerator<Item, void, unknown> {
+//   const sleep = async (ms: number) =>
+//     new Promise((resolve) => setTimeout(resolve, ms));
 
-  for (let i = 0; i < 10; ++i) {
-    await sleep(1000);
-    yield {
-      key: `key${i}`,
-      value: `value${i}`,
-    };
-  }
-}
+//   for (let i = 0; i < 10; ++i) {
+//     await sleep(1000);
+//     yield {
+//       key: `key${i}`,
+//       value: `value${i}`,
+//     };
+//   }
+// }
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
-  const [hostParam, portParam, usernameParam, passwordParam, domainParam] = [
+  const [
+    hostParam,
+    portParam,
+    usernameParam,
+    passwordParam,
+    domainParam,
+    widthParams,
+    heightParam,
+    bitsPerPixelParam,
+  ] = [
     searchParams.get("host"),
     searchParams.get("port"),
     searchParams.get("username"),
     searchParams.get("password"),
     searchParams.get("domain"),
+    searchParams.get("width"),
+    searchParams.get("height"),
+    searchParams.get("bitsPerPixel"),
   ];
 
-  const [host, port, username, password, domain] = [
+  const [host, port, username, password, domain, width, height, bitsPerPixel] = [
     hostParam,
     parseInt(portParam || "3389"),
     usernameParam,
     passwordParam,
     domainParam,
+    parseInt(widthParams || "256"),
+    parseInt(heightParam || "256"),
+    parseInt(bitsPerPixelParam || "8"),
   ];
 
   console.log(
-    "host, port, username, password, domain",
+    "host, port, username, password, domain, width, height, bitsPerPixel",
     host,
     port,
     username,
     password,
-    domain
+    domain,
+    width,
+    height,
+    bitsPerPixel
   );
 
   if (!host) {
@@ -95,8 +116,9 @@ export async function GET(req: NextRequest) {
     password,
     domain: domain || undefined,
     // certIgnore: true,
-    width: 512,
-    height: 512,
+    width,
+    height,
+    bitsPerPixel,
     // bitsPerPixel: 8,
 
     // enablePerf: true,
@@ -108,25 +130,17 @@ export async function GET(req: NextRequest) {
   })
     .on("connect", function () {
       console.log("CONNECTED");
-      // stream = new WritableStream();
     })
-    .on("close", function () {
-      console.log("CLOSED");
-      // reader.close();
-      // writer.close();
+    .on("close", function (reason: any) {
+      console.log("CLOSED", reason);
       stream.end();
     })
     .on("bitmap", function (bitmap: any) {
-      console.log("BITMAP", bitmap);
-      // writer.getWriter().write(bitmap);
+      console.log("BITMAP", {...bitmap, buffer: bitmap.buffer.length});
       stream.write(Buffer.from(JSON.stringify(bitmap)));
-      // stream.write(Buffer.from(JSON.stringify({ lol: "ilol" })));
     })
     .on("error", function (err: any) {
       console.log("error", err);
-    })
-    .on("ERROR", function (err: any) {
-      console.log("ERRRROOOOOOOR");
     });
   client.connect();
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,9 @@ export default function () {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [domain, setDomain] = useState("");
+  const [width, setWidth] = useState(256);
+  const [height, setHeight] = useState(256);
+  const [bitsPerPixel, setBitsPerPixel] = useState(32);
 
   const connect = async () => {
     const url = new URL("/api/", document.location.origin);
@@ -42,31 +45,44 @@ export default function () {
     url.searchParams.append("username", username);
     url.searchParams.append("password", password);
     url.searchParams.append("domain", domain);
+    url.searchParams.append("width", width.toString());
+    url.searchParams.append("height", height.toString());
+    url.searchParams.append("bitsPerPixel", bitsPerPixel.toString());
     // await fetch(url);
     while (true) {
       const it = streamingFetch(url);
-      for await (let value of it) {
+      let toparse = "";
+      for await (let chunk of it) {
         try {
-          console.log("v", value);
-          const chunk = JSON.parse(value);
-          console.log("chunk", chunk.buffer.data.length);
-          const lol = new Uint8ClampedArray(4 * chunk.w * chunk.h);
-          for (let i = 0; i < chunk.buffer.data.length; i += 4) {
-            lol[i + 0] = chunk.buffer.data[i];
-            lol[i + 1] = chunk.buffer.data[i + 1];
-            lol[i + 2] = chunk.buffer.data[i + 2];
-            lol[i + 3] = chunk.buffer.data[i + 3];
+          console.log("Chunk size", chunk.length);
+          toparse += chunk;
+          console.log("To parse size", toparse.length);
+          const parsed = JSON.parse(toparse);
+          toparse = "";
+          console.log("Parsed size", parsed.buffer.data.length);
+          // console.log("Parsed", parsed.buffer.data);
+          // BGRA -> RGBA
+          const arr = new Uint8ClampedArray(4 * parsed.w * parsed.h);
+          for (let i = 0; i < parsed.buffer.data.length; i += 4) {
+            arr[i + 0] = parsed.buffer.data[i + 2];
+            arr[i + 1] = parsed.buffer.data[i + 1];
+            arr[i + 2] = parsed.buffer.data[i];
+            arr[i + 3] = parsed.buffer.data[i + 3];
           }
-          console.log("lol", lol);
-          // var buffer32 = new Uint32Array(chunk.buffer),
-          //   len = buffer32.length;
-          // while (len--) buffer32[len] = chunk.buffer[len];
+          // console.log("arr", arr);
+          const imageData = new ImageData(arr, parsed.w, parsed.h);
+          // console.log("imageData", imageData);
           canvasRef.current
             ?.getContext("2d")
-            ?.putImageData(new ImageData(lol, chunk.w, chunk.h), chunk.x, chunk.y);
-          // setData((prev) => [...prev, chunk]);
-        } catch (e: any) {
-          console.warn(e.message);
+            ?.putImageData(imageData, parsed.x, parsed.y);
+        } catch (e) {
+          // console.log(e);
+          const lol =
+            toparse.substring(0, 10) +
+            " ... " +
+            toparse.substring(toparse.length - 10, toparse.length);
+          // console.log(lol);
+          // console.log(chunk)
         }
       }
     }
@@ -79,12 +95,14 @@ export default function () {
           label="host"
           value={host}
           onChange={(event) => setHost(event.target.value)}
+          sx={{ flex: 1 }}
         />
         <TextField
           type="number"
           label="port"
           value={port}
           onChange={(event) => setPort(parseInt(event.target.value))}
+          sx={{ flex: 1 }}
         />
       </Stack>
       <Stack direction="row" gap={2}>
@@ -92,12 +110,14 @@ export default function () {
           label="Username"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
+          sx={{ flex: 1 }}
         />
         <TextField
           type="password"
           label="Password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          sx={{ flex: 1 }}
         />
       </Stack>
       <TextField
@@ -107,7 +127,7 @@ export default function () {
       />
       <Button onClick={() => connect()}>Connect</Button>
 
-      <canvas ref={canvasRef} width={1024} height={768} />
+      <canvas ref={canvasRef} width={width} height={height} />
     </Stack>
   );
 }
